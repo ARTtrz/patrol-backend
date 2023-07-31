8f8105bba92d04b95ffb333ebdfd9498bdd24143
Artyom <@artemkim953@gmail.com>
2023-07-31 22:30:17 +0600

    final2

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('server');
+  app.enableCors({
+    origin: ['https://patrolai.vercel.app/'],
+    methods: 'GET, POST',
+    credentials: true,
+  });
   await app.listen(8000);
 }
 bootstrap();
